a9eea920c83708e0fa8c052bdebd01b9696ae979
dcode <dr.glasdou@gmail.com>
2022-07-29 09:58:08 -0600

    done: Compound Component section

---
+++ 03-compound-component-pattern/src/components/ProductImage.tsx
@@ -0,0 +1,13 @@
+import { useContext } from 'react'
+
+import noImage from '../assets/no-image.jpg'
+import styles from '../styles/styles.module.css'
+import { ProductContext } from './ProductCard'
+
+export const ProductImage = ({ img = '' }) => {
+	const { product } = useContext(ProductContext)
+
+	const image = img || product.img || noImage
+
+	return <img src={image} alt='Product' className={styles.productImg} />
+}
