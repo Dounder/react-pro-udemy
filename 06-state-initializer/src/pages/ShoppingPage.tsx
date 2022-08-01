6f6141ec0ee893c1df8f4d04154502e6ab51ce01
dcode <dr.glasdou@gmail.com>
2022-08-01 15:17:48 -0600

    done: state initializer section

---
+++ 06-state-initializer/src/pages/ShoppingPage.tsx
@@ -0,0 +1,39 @@
+import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components'
+import { products } from '../data/products'
+import '../styles/custom-styles.css'
+
+const product = products[0]
+
+export const ShoppingPage = () => {
+	return (
+		<div>
+			<h1>ShoppingPage</h1>
+			<hr />
+
+			<ProductCard
+				product={product}
+				className='bg-dark text-white'
+				initialValues={{
+					count: 4,
+					maxCount: 10,
+				}}
+			>
+				{({ reset, increaseBy, count, isMaxCountReached }) => (
+					<>
+						<ProductImage
+							className='custom-image'
+							style={{ boxShadow: '10px 10px 10px rgba(0, 0, 0,0.2)' }}
+						/>
+						<ProductTitle className='text-bold' />
+						<ProductButtons className='custom-buttons' />
+
+						<button onClick={() => reset()}> Reset </button>
+						<button onClick={() => increaseBy(-2)}> -2 </button>
+						{!isMaxCountReached && <button onClick={() => increaseBy(2)}> +2 </button>}
+						<span>{count}</span>
+					</>
+				)}
+			</ProductCard>
+		</div>
+	)
+}
